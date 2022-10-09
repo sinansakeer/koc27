de51fc59b8b27e337cbf63e9e8ef60808461bbb9
sinansakeer <115392311+sinansakeer@users.noreply.github.com>
2022-10-09 12:53:35 +0530

    Add files via upload

---
+++ calculator.py.py
@@ -0,0 +1,10 @@
+a = int(input("please enter number 1 "))
+b = int(input("please enter number 2 "))
+
+print("addition is ", a+b)
+print("substraction is ", a-b)
+print("multiplication is ", a*b)
+print("division is ", a/b )
+print("exponentiation is ", a**b)
+print("floor division is ", a//b)
+print("modulus is ", a%b)
